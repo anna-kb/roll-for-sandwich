73cb0c08edece654aa3395f8c37c45f44c1e1dde
anna-kb <129875026+anna-kb@users.noreply.github.com>
2023-05-01 08:41:55 -0600

    image

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
             </div>
             <img
               className={"roll-image" + getShake()}
-              src="/assets/d20_dice.png"
+              src="/d20_dice.png"
               alt="d20 Dice"
             ></img>
           </div>
